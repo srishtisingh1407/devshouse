448887a670d4f6f7864e71dff42ee655645598c7
Srishtisingh1407 <srishtisingh140704@gmail.com>
2024-03-12 00:42:23 +0530

    Changes in js file

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,9 @@
-// Get the current page from your application logic
-var currentPage = "/index"; // Update this with the actual current page
 
-// Select the corresponding navigation item and add a class
+var currentPage = "/index"; 
+
 var navigationItems = document.querySelectorAll('.nav__content ul li a');
 navigationItems.forEach(function(item) {
   if (item.getAttribute('href') === currentPage) {
-    item.classList.add('active'); // Add your custom class name, e.g., 'active'
+    item.classList.add('active'); 
   }
 });
