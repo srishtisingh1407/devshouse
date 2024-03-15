5830ce606c43366cbc870a91c8b4e279ef3a29d1
Srishtisingh1407 <srishtisingh140704@gmail.com>
2024-03-15 21:58:17 +0530

    create login-signup

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+  router.get('/', function(req, res, next) {
+    res.render('index');
+  });
+  router.get('/timeline', function(req, res, next) {
+    res.render('timeline');
+  });
 
-router.get('/login-signup', function(req, res, next) {
-  res.render('login-signup');
-});
+  router.get('/login-signup', function(req, res, next) {
+    res.render('login-signup');
+  });
 
 module.exports = router;
