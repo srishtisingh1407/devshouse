9c3c1d941b55ca04670044aa88e9746212462272
Shreya-9305 <shreyapandey9305@gmail.com>
2024-03-16 18:26:28 +0530

    add myprofile

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,15 @@ router.get('/login-signup', function(req, res, next) {
   res.render('login-signup');
 });
 
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
+});
+
+router.get('/myprofile', function(req, res, next) {
+  res.render('myprofile');
+});
+
+router.get('/tab', function(req,res,next){
+res.render('tab')
+});
 module.exports = router;
